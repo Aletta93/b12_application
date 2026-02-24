14df8df2787df372e2067f49a0918fb1b441c709
Aletta93 <klopperaletta@gmail.com>
2026-02-24 15:32:02 +0100

    test with correct json

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import hmac
 import hashlib
 
 HASH_SECRET_KEY = os.environ["hash_secret_key"]
-JSON_PAYLOAD = {"timestamp": "ISO8601","name": "Aletta Klopper","email": "klopperaletta@gmail.com","resume_link": "https://www.linkedin.com/in/aletta-klopper-18bbba42/","action_run_link": "some action link"}
+#JSON_PAYLOAD = {"timestamp": "ISO8601","name": "Aletta Klopper","email": "klopperaletta@gmail.com","resume_link": "https://www.linkedin.com/in/aletta-klopper-18bbba42/","action_run_link": "some action link"}
+JSON_PAYLOAD = {"action_run_link":"https://link-to-github-or-another-forge.example.com/your/repository/actions/runs/run_id","email":"you@example.com","name":"Your name","repository_link":"https://link-to-github-or-other-forge.example.com/your/repository","resume_link":"https://pdf-or-html-or-linkedin.example.com","timestamp":"2026-01-06T16:59:37.571Z"}
 POST_URL = "https://b12.io/apply/submission"
 
 def main():
