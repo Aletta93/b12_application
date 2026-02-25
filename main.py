399cc5746854c88132c5199681d751f15e42f987
Aletta93 <klopperaletta@gmail.com>
2026-02-25 09:41:31 +0100

    test

--- main.py
+++ main.py
@@ -5,27 +5,26 @@ import hmac
 import hashlib
 
 HASH_SECRET_KEY = os.environ["hash_secret_key"]
-#JSON_PAYLOAD = {"timestamp": "ISO8601","name": "Aletta Klopper","email": "klopperaletta@gmail.com","resume_link": "https://www.linkedin.com/in/aletta-klopper-18bbba42/","action_run_link": "some action link"}
-JSON_PAYLOAD = {"action_run_link":"https://link-to-github-or-another-forge.example.com/your/repository/actions/runs/run_id","email":"you@example.com","name":"Your name","repository_link":"https://link-to-github-or-other-forge.example.com/your/repository","resume_link":"https://pdf-or-html-or-linkedin.example.com","timestamp":"2026-01-06T16:59:37.571Z"}
+JSON_PAYLOAD = '{"timestamp":"2026-02-24T12:03:50Z","name":"Aletta Klopper","email":"klopperaletta@gmail.com","resume_link":"https://www.linkedin.com/in/aletta-klopper-18bbba42/","action_run_link":"some action link"}'
+#JSON_PAYLOAD = '{"action_run_link":"https://link-to-github-or-another-forge.example.com/your/repository/actions/runs/run_id","email":"you@example.com","name":"Your name","repository_link":"https://link-to-github-or-other-forge.example.com/your/repository","resume_link":"https://pdf-or-html-or-linkedin.example.com","timestamp":"2026-01-06T16:59:37.571Z"}'
 POST_URL = "https://b12.io/apply/submission"
 
 def main():
-    headers = {
-        "Content-Type": "application/json",
-        "X-Signature-256": "sfdsdf"
-    }
-    message = json.dumps(JSON_PAYLOAD)
-
-    #submission_request = requests.post(url=POST_URL, headers=headers, data=JSON_PAYLOAD)
 
     signature = hmac.new(
         HASH_SECRET_KEY.encode("utf-8"),
-        msg=message.encode("utf-8"),
+        msg=JSON_PAYLOAD.encode("utf-8"),
         digestmod=hashlib.sha256
     ).hexdigest()
 
     print(f"SIGNATURE: {signature}")
     
+    headers = {
+        "Content-Type": "application/json",
+        "X-Signature-256": signature
+    }
+
+    #submission_request = requests.post(url=POST_URL, headers=headers, data=JSON_PAYLOAD)
 
 if __name__ == '__main__':
     main()
